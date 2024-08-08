b752a2fa33fd765492aa7fac817ae14d24fd05ab
Valentin <val.tuznichenko@yandex.ru>
2024-08-08 11:29:38 +0700

    3

--- cert_emp/main.tf
+++ cert_emp/main.tf
@@ -107,7 +107,6 @@ resource "local_file" "ansible_inventory" {
   content  = <<EOT
             [cert_emp]
             ${join("\n", yandex_compute_instance.vm-1.network_interface.0.nat_ip_address)}
-            ${join("\n", yandex_compute_instance.vm-2.network_interface.0.nat_ip_address)}
   EOT
 
 }
